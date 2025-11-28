ca46954ba86e0c78a7c4d0137021d70a9460cc6a
maskSnorkelFins <icodeandmakevideos@gmail.com>
2025-11-28 08:54:28 -0500

    added multiplayer logic

--- server.js
+++ server.js
@@ -13,15 +13,65 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 
+
+// game state
+let players = {}; // { socket.id: {name, score} }
+let submissions = []; // [ {playerId, word, score} ]
+
+
 // handle socket connections
 io.on('connection', (socket) => {
 	console.log("connected user: ", socket.id);
 
+	// player joins
+	socket.on('joinGame', (name) => {
+		players[socket.id] = { name, score: 0 };
+		console.log(`joined player: ${name}`);
+		io.emit('updatePlayers', players); // broadcast updated player list
+	});
+	// player disconnects
+	socket.on('disconnect', () => {
+		console.log(`disconnected player: ${socket.id}`);
+		delete players[socket.id];
+		io.emit('updatePlayers', players); // broadcast updated player list
+	});
+
 	// listen for word from client
 	socket.on('submitWord', (word) => {
-		const score = scoreWord(word);
-		socket.emit('scoreResult', { word, score }); // send result back to client
+		//
+		// reject words longer than N characters (30?)
+		//
+		// track room.currentPlayerId
+		// reject submissions from anyone else
+		// if (socket.id !== room.currentPlayerId) return reject();
+		//
+		// server rate limiting – 1 submission every X seconds, reject on spam
+		// timestamp each turn to prevent “clock rewinding” attempts
+		//
+		// only broadcast words after they are finalized
+		//
+		// 
+		//
+		const player = players[socket.id];
+		if (!player) return; // safety check
+
+		const wordScore = scoreWord(word);
+		player.score += wordScore;
+
+		const submission = {
+			playerId: socket.id,
+			name: player.name,
+			word,
+			score: wordScore
+		};
+		submissions.push(submission);
+
+		io.emit('newSubmission', submission); // broadcast submission to all clients
+		io.emit('updatePlayers', players); // broadcast updated player scores
+		// socket.emit('scoreResult', { word, score }); // send result back to client
 	});
+
+
 });
 
 
